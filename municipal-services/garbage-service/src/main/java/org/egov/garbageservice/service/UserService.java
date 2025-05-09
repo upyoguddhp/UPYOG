918ae85f01fcad8aa296dffc800b05c4dc98a055
Divsharma2407 <divsharma.ext@deloitte.com>
2025-05-09 12:26:12 +0530

    Update UserService.java

--- municipal-services/garbage-service/src/main/java/org/egov/garbageservice/service/UserService.java
+++ municipal-services/garbage-service/src/main/java/org/egov/garbageservice/service/UserService.java
@@ -230,6 +230,8 @@ public class UserService {
 		userSearchRequest.setUserType(owner.getType());
 		userSearchRequest.setName(owner.getName());
 
+		log.info("Owner Name:" +owner.getName());
+
 		StringBuilder uri = new StringBuilder(grbgConfig.getUserServiceHostUrl())
 				.append(grbgConfig.getUserSearchEndpoint());
 		return userCall(userSearchRequest, uri);
