b8f902fbc7b8549c203e312e2ee9bdbb3b505f12
Sharma <jatsharma.ext@deloitte.com>
2026-01-08 17:09:24 +0530

    added property preview

---
+++ municipal-services/property-services/src/main/java/org/egov/pt/models/CalculateTaxPreviewResponse.java
@@ -0,0 +1,25 @@
+package org.egov.pt.models;
+import java.math.BigDecimal;
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CalculateTaxPreviewResponse {
+
+    private String propertyId;
+    private String tenantId;
+    private String oldPropertyId;
+    private Object address;  
+    private BigDecimal totalAnnualTax;
+    private BigDecimal taxForPeriod;
+    private BigDecimal rebateAmount;
+    private BigDecimal days;
+
+    private JsonNode calculationDetails;
+}
