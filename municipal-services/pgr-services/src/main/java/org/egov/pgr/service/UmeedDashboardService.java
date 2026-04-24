0498126b8fd2b968618ebe336fe63f2f3594904f
Rahul <rahul1.ext@deloitte.com>
2026-04-24 17:37:21 +0530

    PGR Changes

--- municipal-services/pgr-services/src/main/java/org/egov/pgr/service/UmeedDashboardService.java
+++ municipal-services/pgr-services/src/main/java/org/egov/pgr/service/UmeedDashboardService.java
@@ -49,32 +49,61 @@ public class UmeedDashboardService {
 
 		// String yesterday = "30-08-2025";
 		
-		LocalDate startDate = LocalDate.parse("01-04-2025", formatter);
-		LocalDate endDate = startDate;
+//		LocalDate startDate = LocalDate.parse("01-04-2025", formatter);
+//		LocalDate endDate = startDate;
 
 		// Define the month (August 2025)
 //    		LocalDate startDate = yesterday;
 //    		LocalDate endDate = yesterday;
 		// LocalDate endDate = startDate.withDayOfMonth(startDate.lengthOfMonth());
+		
+		
+		
+		LocalDate startDate = LocalDate.parse("01-02-2026", formatter);
+		LocalDate endDate   = LocalDate.parse("11-02-2026", formatter);
 
 		List<DataItem> allProcessedItems = new ArrayList<>();
-
+		
 		for (LocalDate date = startDate; !date.isAfter(endDate); date = date.plusDays(1)) {
-			String formattedDate = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
 
-			List<DataItem> dataItems = PGRRepository.getUniqueWards(formattedDate);
+		    String formattedDate = date.format(formatter);
 
-			if (CollectionUtils.isEmpty(dataItems)) {
-				continue;
-			}
+		    List<DataItem> dataItems = PGRRepository.getUniqueWards(formattedDate);
 
-			List<DataItem> processedItems = dataItems.stream()
-					.map(dataItem -> buildDataItemMetrics(dataItem, formattedDate, slaDays))
-					.collect(Collectors.toList());
+		    if (CollectionUtils.isEmpty(dataItems)) {
+		        continue;
+		        
+		    }
 
-			allProcessedItems.addAll(processedItems);
+		    for (DataItem dataItem : dataItems) {
 
+		        try {
+		            DataItem result = buildDataItemMetrics(dataItem, formattedDate, slaDays);
+		            allProcessedItems.add(result);
+		        } catch (Exception e) {
+		            System.out.println("Error for date: " + formattedDate);
+		        }
+		    }
 		}
+
+//		List<DataItem> allProcessedItems = new ArrayList<>();
+
+//		for (LocalDate date = startDate; !date.isAfter(endDate); date = date.plusDays(1)) {
+//			String formattedDate = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+//
+//			List<DataItem> dataItems = PGRRepository.getUniqueWards(formattedDate);
+//
+//			if (CollectionUtils.isEmpty(dataItems)) {
+//				continue;
+//			}
+//
+//			List<DataItem> processedItems = dataItems.stream()
+//					.map(dataItem -> buildDataItemMetrics(dataItem, formattedDate, slaDays))
+//					.collect(Collectors.toList());
+//
+//			allProcessedItems.addAll(processedItems);
+//
+//		}	
 		// List<DataItem> processedItems = dataItems.stream()
 		// .map(dataItem -> buildDataItemMetrics(dataItem, yesterday,
 		// slaDays)).collect(Collectors.toList());
