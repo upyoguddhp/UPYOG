26582ee716f04b74ec3f476b86c4414dc1de01db
jat13sharma <jatsharma.ext@deloitte.com>
2026-06-12 11:54:53 +0530

    added logs for time periods in grbg scheduler

--- municipal-services/garbage-service/src/main/java/org/egov/garbageservice/service/GarbageAccountSchedulerService.java
+++ municipal-services/garbage-service/src/main/java/org/egov/garbageservice/service/GarbageAccountSchedulerService.java
@@ -380,7 +380,7 @@ public class GarbageAccountSchedulerService {
 				log.info("newTo {}",newTo);
 				log.info("existingFrom {}",existingFrom);
 				log.info("existingTo {}",existingTo);
-				log.info("Bill: ", billResponse.getBill());	
+				log.info("Bill: {}", billResponse.getBill().get(0).getId());	
 				
 				if (isOverlapping(newFrom, newTo, existingFrom, existingTo)) {
 					throw new CustomException("BILL_PERIOD_OVERLAP", "Bill already exists for overlapping period");
