8f0a77a3809dcf0122c63c48419e2d36913c075b
jat13sharma <jatsharma.ext@deloitte.com>
2026-08-06 16:52:53 +0530

    Added global exceptions in billing, collection, apportion, pg, shortening, workflow

---
+++ core-services/egov-workflow-v2/src/main/java/org/egov/wf/exception/ErrorInfo.java
@@ -0,0 +1,33 @@
+package org.egov.wf.exception;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+public class ErrorInfo {
+	private String errorCode;
+	private String errorMessage;
+	private List<String> errorDetails;
+	private Date timestamp;
+
+	public ErrorInfo(String errorCode, String errorMessage, Date timestamp) {
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+		this.timestamp = timestamp;
+		this.errorDetails = Arrays.asList(errorMessage);
+	}
+
+	public ErrorInfo(String errorCode, String errorMessage, List<String> errors, Date timestamp) {
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+		this.timestamp = timestamp;
+		this.errorDetails = errors;
+	}
+}
